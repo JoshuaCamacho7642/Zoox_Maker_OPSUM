1edf08b0a7bd8b206eaea4f3b24d1e6577e431c2
Joshua Camacho - Laptop <joshua.camacho@zooxsmart.com>
2024-03-31 13:55:39 -0600

    Update table register view + computed and reactive data
    Some function and new elements was added to the view also computed and reactive data was added by update data in the view.

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,14 +1,112 @@
 <template>
-  <div>
-    <h1>
+  <div class="columns m-0">
+    <div class="column is-12">
+      <table class="table is-fullwidth has-background-light">
+        <thead>
+        <tr>
+          <th><abbr title="Position">#No.</abbr></th>
+          <th>Nombre</th>
+          <th><abbr title="Lastname">Apellido</abbr></th>
+          <th><abbr title="Email">Correo</abbr></th>
+          <th><abbr title="Company">Compania</abbr></th>
+          <th><abbr title="Aprove">Aceptar</abbr></th>
+          <th><abbr title="Deny">Denegar</abbr></th>
+        </tr>
+        </thead>
 
-    </h1>
-    <button @click="userStore.registeruser('ignacio')">
-      Ingresar
-    </button>
+        <tbody>
+        <tr v-for="(newUser, index) in newUserArray" :key="index"
+            :class="{'is-selected': reacData.rowStatus && ( reacData.rowIndex === index ) }"
+            @mouseover="reacData.rowStatus = true; reacData.rowIndex = index;"
+            @mouseleave="reacData.rowStatus = false; reacData.rowIndex = index;">
+          <th>{{ index + 1 }}</th>
+          <td>{{ newUser.firstName }}</td>
+          <td>{{ newUser.lastName }}</td>
+          <td>{{ newUser.mail }}</td>
+          <td>{{ newUser.company }}</td>
+          <td>
+            <button class="button is-active" @click="userStore.registeruser('ignacio')">
+              Aceptar
+            </button>
+          </td>
+          <td>
+            <button class="button is-danger" @click="userStore.registeruser('ignacio')">
+              Cancelar
+            </button>
+          </td>
+        </tr>
+        </tbody>
+        <tfoot class="is-hidden">
+        <tr>
+          <th><abbr title="Position">#No.</abbr></th>
+          <th>Nombre</th>
+          <th><abbr title="Lastname">Apellido</abbr></th>
+          <th><abbr title="Email">Correo</abbr></th>
+          <th><abbr title="Company">Compania</abbr></th>
+          <th><abbr title="Aprove">Aceptar</abbr></th>
+          <th><abbr title="Deny">Denegar</abbr></th>
+        </tr>
+        </tfoot>
+      </table>
+      <nav class="pagination" role="navigation" aria-label="pagination">
+        <a href="#" class="pagination-previous">Previous</a>
+        <a href="#" class="pagination-next">Next page</a>
+        <ul class="pagination-list">
+          <li v-for="indexPag in 10 " :key="indexPag">
+            <a href="#"
+               class="pagination-link"
+               :class="{ 'is-current' : indexPag === 1 }"
+               aria-label="Goto page 1">
+              {{ indexPag }}
+            </a>
+          </li>
+          <li>
+            <span class="pagination-ellipsis">&hellip;</span>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
 <script setup>
+import { reactive, computed } from "vue"
 import { useUserStore } from "@/stores/user.js"
-const userStore = useUserStore();
+
+const userStore = useUserStore()
+
+let reacData = reactive({
+  rowIndex: undefined,
+  rowStatus: false
+})
+
+const compData = computed(() => {
+  return reacData;
+})
+
+const newUserArray = [
+  {
+    firstName:"Ignacio",
+    lastName:"Martinez",
+    mail:"imartinez@jdco.com",
+    company:"JD Company",
+  },
+  {
+    firstName:"Joshua",
+    lastName:"Camacho",
+    mail:"JACR@Pineoak.com",
+    company:"Pineoak Studio",
+  },
+  {
+    firstName:"Samuel",
+    lastName:"Ramirez",
+    mail:"sramirez@jdco.com",
+    company:"JD Company",
+  },
+  {
+    firstName:"Bernardo",
+    lastName:"Robles",
+    mail:"bernardo.robles@bernico.com",
+    company:"BernyCo",
+  },
+]
 </script>
